6ef6b91dccc1dc177eac9860f56895639c17bf8b
donarudo0413 <96948798+donarudo0413@users.noreply.github.com>
2022-01-02 13:55:56 +0900

    Add files via upload

---
+++ csv.py
@@ -0,0 +1,19 @@
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.zeros(10)
+y = np.zeros(10)
+c = 0
+with open('data2.csv', 'r') as csvfile:
+#    csv_reader = csv.reader(csvfile, delimiter='\t', quotechar='"')
+    csv_reader = csv.reader(csvfile)
+    next(csv_reader)#ヘッダーを消す
+    for row in csv_reader:
+        x[c] = row[0]
+        y[c] = row[1]
+        c += 1
+print('x=',x)
+print('---')
+print('y=',y)
+plt.plot(x,y)
